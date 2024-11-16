424822076925bcdf346323633364f0c64f5e64b6
youssefM <youssef.ait.malek1@gmail.com>
2024-11-17 00:27:56 +0100

    hero form adjustments

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,18 +1,99 @@
-import React from 'react'
+import React from "react";
 
 function Hero() {
   return (
-    <section className="flex items-center justify-center h-screen  bg-cover bg-center text-white bg-hero-image rounded-xl mx-4" >
-    <div className="text-center max-w-md px-4">
-      <h1 className="text-5xl md:text-6xl font-bold mb-4">Welcome to My Next.js Site</h1>
-      <p className="text-lg md:text-xl mb-8">Your journey to learn web development starts here!</p>
-      <button className="px-6 py-3 text-lg bg-blue-500 hover:bg-blue-700 rounded-lg transition duration-300">
-        Get Started
-      </button>
-    </div>
-    <div>this a form</div>
-  </section>
-  )
+    <section className="flex flex-col md:flex-row   h-screen  bg-cover bg-center text-white bg-hero-image rounded-xl mx-4">
+      <div className="text-center  px-4 w-1/2">
+        <h1 className="text-5xl md:text-6xl font-bold mb-4">
+          Welcome to My Next.js Site
+        </h1>
+        <p className="text-lg md:text-xl mb-8">
+          Your journey to learn web development starts here!
+        </p>
+        <button className="px-6 py-3 text-lg bg-blue-500 hover:bg-blue-700 rounded-lg transition duration-300">
+          Get Started
+        </button>
+      </div>
+      <div className="flex-col  bg-yellow-50 p-4 rounded-lg w-1/2 h-full ">
+        <div className="">
+          <form className="max-w-sm mx-auto">
+            <div className="mb-5">
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                placeholder="name@flowbite.com"
+                required
+              />
+            </div>
+            <div className="mb-5">
+              <label
+                htmlFor="password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                required
+              />
+            </div>
+            <div className="mb-5">
+              <label
+                htmlFor="repeat-password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Repeat password
+              </label>
+              <input
+                type="password"
+                id="repeat-password"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                required
+              />
+            </div>
+            <div className="flex items-start mb-5">
+              <div className="flex items-center h-5">
+                <input
+                  id="terms"
+                  type="checkbox"
+                  value=""
+                  className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                  required
+                />
+              </div>
+              <label
+                htmlFor="terms"
+                className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                I agree with the{" "}
+                <a
+                  href="#"
+                  className="text-blue-600 hover:underline dark:text-blue-500"
+                >
+                  terms and conditions
+                </a>
+              </label>
+            </div>
+            <button
+              type="submit"
+              className="text-white bg-black hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Register new account
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
